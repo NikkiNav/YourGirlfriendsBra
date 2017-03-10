5b4c47c18442501d78cb070e6b45d577645d99b3
Aliya Rahman <anavaroli@gmail.com>
2017-03-10 17:59:39 -0500

    added brasorter.py and initial name question

---
+++ brasorter.py
@@ -0,0 +1,6 @@
+# write out "what is your name?"
+# wait for person to type their input
+# copy their input to clipboard 
+thispersonsname = raw_input("what is your name? ") 
+# print "Hello, their input"
+print "Hello, " + thispersonsname 
